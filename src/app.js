bab47b68d83625ef4344600a737e170ef9199930
morqan <qurbanbeyli.murad@gmail.com>
2020-10-16 00:30:38 +0400

    refactoring

--- src/app.js
+++ src/app.js
@@ -1,1 +1,39 @@
+import {isValid, createModal} from "./utils";
+import {Question} from "./question";
 import './styles.css'
+
+const modalBtn = document.getElementById('modalBtn')
+const form = document.getElementById('form')
+const input = form.querySelector('#question-input')
+const submitBtn = form.querySelector('#submit')
+
+
+window.addEventListener('load',Question.renderList)
+form.addEventListener('submit',submitFormHandler)
+input.addEventListener('input',()=>{
+    submitBtn.disabled = !isValid(input.value)
+})
+modalBtn.addEventListener('click', openModal)
+
+function submitFormHandler(event) {
+    event.preventDefault()
+    console.log(isValid(input.value))
+    if (isValid(input.value)){
+        const question = {
+            text: input.value.trim(),
+            date: new Date().toJSON()
+        }
+        submitBtn.disabled = true
+        //async request to server
+        Question.create(question)
+        console.log('question:',question)
+        input.value = ''
+        input.className = ''
+        submitBtn.disabled = false
+    }
+
+}
+
+function openModal() {
+    createModal('Authorization',`<h1>test modal</h1>`)
+}
--- src/question.js
+++ src/question.js
@@ -1,5 +1,46 @@
 export class Question {
     static create(question) {
-
+        fetch('https://podcast-frontend-questions-app.firebaseio.com/questions.json',{
+            method: 'POST',
+            body: JSON.stringify(question),
+            headers: {
+                'Content-type': 'applications/json'
+            }
+        }).then(response => response.json())
+          .then(response => {
+              question.id = response.name
+              return question
+                console.log(response)
+          })
+          .then(addToStorage)
+            .then(Question.renderList)
     }
+
+    static renderList(){
+        const questions = getQuestionsFromStorage()
+        const html = questions.length ? questions.map(toCard).join('')
+                    : `<div class="mui--text-headline"> You have not a questions...</div>`
+        const list = document.getElementById('list')
+        list.innerHTML = html
+    }
+}
+function addToStorage(question) {
+    const allQuestions = getQuestionsFromStorage()
+    allQuestions.push(question)
+    localStorage.setItem('questions', JSON.stringify(allQuestions))
+}
+
+function getQuestionsFromStorage() {
+    return JSON.parse(localStorage.getItem('questions') || '[]')
+}
+
+function toCard(question) {
+    return `
+        <div class="mui--text-black-54">
+            ${new Date(question.date).toLocaleDateString()}
+            ${new Date(question.date).toLocaleTimeString()}
+        </div>
+        <div>${question.text} </div>
+        <br>
+    `
 }
